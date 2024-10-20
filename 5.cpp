3a361809e94959bb90ec24596a098901e1999715
Aditya-shivale121 <shivaleaditya22@gmail.com>
2024-10-20 22:54:44 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+template<class T>
+class sorting 
+{
+	public:
+		T a[];
+    int n;
+    void accept()
+    {
+    	for(i=0;i<n;i++)
+        {
+        	cin>>a[i];
+        }
+    void display ()
+    {
+    	for(i=0;i<n;i++)
+    	{
+    		cout<<endl<<a[i];
+		}
+    void sort()
+    {
+    	for(i=0;i<n;i++)
+    	{
+    		for(j=i+1;j<n;j++)
+    		{
+    			if(a[j]<a[i])
+			}
+		}
+	}
+	}
+};
